02705dc21b05f92777f641d62497bc27a95bbbfb
lfcamara <leo.camara@gmail.com>
2021-01-12 12:15:05 -0300

    Observer Pattern

---
+++ Behavioral/Observer/index.js
@@ -0,0 +1,28 @@
+var Store = require('./Store');
+var Shopper = require('./Shopper');
+var Mall = require('./Mall');
+
+var catsAndThings = new Store("Cats & Things");
+var insAndOuts = new Store("Ins and Outs");
+
+var alex = new Shopper("Alex");
+var eve = new Shopper("Eve");
+var sharon = new Shopper("Sharon");
+var mike = new Shopper("Mike");
+
+var valleyMall = new Mall();
+
+catsAndThings.subscribe(alex);
+catsAndThings.subscribe(eve);
+catsAndThings.subscribe(mike);
+// Qualquer objeto pode "observar" o objeto que será "subscribed"
+catsAndThings.subscribe(valleyMall);
+
+insAndOuts.subscribe(sharon);
+insAndOuts.subscribe(valleyMall);
+
+// Ao "anunciar", todos os observers deverao ser notificados
+catsAndThings.sale(20);
+insAndOuts.sale(50);
+
+console.log(valleyMall.sales);
