3f12cce70398e6edb3c186604b9f4f638b915083
lfcamara <leo.camara@gmail.com>
2021-01-13 10:43:41 -0300

    Strategy Pattern

---
+++ Behavioral/Strategy/LogStrategy.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var { appendFile } = require('fs');
+
+class LogStrategy {
+
+  static noDate(timestamp, message) {
+    console.log(message);
+  }
+
+  static toFile(timestamp, message) {
+    var fileName = path.join(__dirname, 'logs.txt');
+    appendFile(fileName, `${timestamp} - ${message} \n`, error => {
+      if (error) {
+        console.log('Error writing to file');
+        console.log(error);
+      }
+    })
+  }
+
+  static toConsole(timestamp, message) {
+    console.log(`${timestamp} - ${message}`);
+  }
+
+  static none() {
+
+  }
+}
+
+module.exports = LogStrategy;
